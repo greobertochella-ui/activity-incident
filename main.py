68089a68295cdb82f4813b8e9de7b2df0e870981
greobertochella-ui <greobertochella@gmail.com>
2026-02-24 11:15:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from routes import create_app
-import os
 import uvicorn
+import app  # importa app.py
+
+def main():
+    uvicorn.run(
+        app.asgi,
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
-asgi = create_app("./static")
+    main()
